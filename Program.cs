c660145272060434f74c741efeff5910998f9c97
Jennipher Medina <jenniphermedina11@gmail.com>
2025-01-18 21:08:02 -0700

    corrected spelling

--- Program.cs
+++ Program.cs
@@ -11,14 +11,14 @@ namespace Wk1Ex2
         static void Main(string[] args)
         {
             Console.WriteLine("What is your first name?"); //prints question
-            string firstName = Console.ReadLine(); //recieves input and assigns it as firstName
+            string firstName = Console.ReadLine(); //receives input and assigns it as firstName
             Console.WriteLine("What is your last name?"); //prints question
-            string lastName = Console.ReadLine(); //recieves input and assigns it as lastName
+            string lastName = Console.ReadLine(); //receives input and assigns it as lastName
 
             Console.WriteLine("What is your age?"); //prints question
-            int age = Convert.ToInt16(Console.ReadLine()); //recieves input, assigns it as age, and converts input to an int type
+            int age = Convert.ToInt16(Console.ReadLine()); //receives input, assigns it as age, and converts input to an int type
             Console.WriteLine("What is your favorite color?"); //prints question
-            string color = Console.ReadLine(); //recieves input and assigns it as color
+            string color = Console.ReadLine(); //receives input and assigns it as color
 
             Console.WriteLine("Hello, " +firstName+ " " +lastName+ "! You are " +age+ " years old and your favorite color is " +color+ "."); //prints phrase with inputted information
         }
